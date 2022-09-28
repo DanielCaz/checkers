42787ca743ac7210c4160a0a8b88551a89b5957a
DanielCaz <cazarez.jorge.prog2019@gmail.com>
2022-09-28 07:36:18 -0500

    Typo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ const Layout = ({ children }: { children: any }) => {
         <title>Checkers</title>
         <meta name="description" content="Online checkers" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
       </Head>
       {children}
     </div>
